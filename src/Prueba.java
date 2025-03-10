50333ac4e733c90bb85344f327ffa30f31eebea2
dannirlasso2005 <danalasso@soy.sena.edu.co>
2025-03-10 18:24:30 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba {
   public static void main(String[] args) {
     System.out.print("Holaaa");
-    System.out.print("hello people");
+    System.out.print("hello nicolle");
     System.out.print("holi");
   }
 

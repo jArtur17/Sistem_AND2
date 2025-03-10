750642853834cfc75b913ae4961c52db8e28a34c
Fliao <nickcorrea@soy.sena.edu.co>
2025-03-10 18:09:11 -0500

    cliente mensaje

--- src/clientes.java
+++ src/clientes.java
@@ -1,2 +1,7 @@
 public class clientes {
+    public static void main(String[] args) {
+        System.out.print("Holaaa");
+        System.out.print("hello people");
+        System.out.print("holi");
+    }
 }

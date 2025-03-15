8cec96ffb74707472b1ffb515069617027a984f8
jArtur17 <arturobanol21@gmail.com>
2025-03-14 21:14:37 -0500

    Agregué conexion, (conectar con base de datos farmacia)

---
+++ src/conexion/ConexionFarmacia.java
@@ -0,0 +1,20 @@
+package conexion;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionFarmacia {
+
+    public Connection getConnection() {
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection("jdbc:mysql//localhost:3306/drogueria", "root", "root");
+        }catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos"+e.toString());
+        }
+        return con;
+    }
+}

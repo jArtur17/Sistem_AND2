1d32a14feab70f83f6af7f9053d1682e0ae030a5
jArtur17 <arturobanol21@gmail.com>
2025-03-16 12:15:23 -0500

    Eliminar archivos

--- src/Farmacia.java
+++
@@ -1,7 +0,0 @@
-public class Farmacia {
-    public static void main(String[] args) {
-        System.out.print("Holaaa");
-        System.out.print("hello people");
-        System.out.print("holi");
-    }
-}

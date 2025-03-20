9e3f60ea953eed379ffa0d11df84be8eca8c7db0
jArtur17 <arturobanol21@gmail.com>
2025-03-20 15:33:12 -0500

    Conexion a la base de datos desde la rama auxiliar

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,4 +1,22 @@
 package Conexion;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+
+    public Connection getConnection() {
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drogueria", "root", "root");
+        }catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos"+e.toString());
+        }
+
+        return con;
+
+    }
 }

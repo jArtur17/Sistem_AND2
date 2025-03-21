fb2c71a76fe9f4ac84e2213ce54bd58425013f08
dannirlasso2005 <danalasso@soy.sena.edu.co>
2025-03-21 12:09:15 -0500

    historial compras

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
         Connection con = null;
 
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drogueria", "root", "root");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drogueria", "root", "");
         }catch(SQLException e) {
             JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos"+e.toString());
         }
---
+++ src/HistorialCompra/HistorialCompraGUI.java
@@ -0,0 +1,8 @@
+package HistorialCompra;
+
+import javax.swing.*;
+
+public class HistorialCompraGUI {
+    private JTable table1;
+    private JButton a️Button;
+}
